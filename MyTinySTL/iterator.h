becba835cd7b406d2e135e27a870ca865b5a68d2
szh-maker <2412192306@qq.com>
2023-11-09 21:16:17 +0800

    iterator.h

--- MyTinySTL/iterator.h
+++ MyTinySTL/iterator.h
@@ -189,8 +189,136 @@ namespace mystl {
     /**************************************************************************************/
     // 类模板：reverse_iterator
     // 反向迭代器，使前进为后退，后退为前进
+    template<typename Iterator>
+    class reverse_iterator {
+    private:
+        Iterator current;   // 记录对应的正向迭代器
     
+    public:
+        // 反向迭代器的五种相应的型别
+        typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+        typedef typename iterator_traits<Iterator>::value_type        value_type;
+        typedef typename iterator_traits<Iterator>::difference_type   difference_type;
+        typedef typename iterator_traits<Iterator>::pointer           pointer;
+        typedef typename iterator_traits<Iterator>::reference         reference;
 
-}
+        typedef Iterator                                              iterator_type;
+        typedef reverse_iterator<Iterator>                            self;
+    
+    public:
+        // 构造函数
+        reverse_iterator() {}
+        explicit reverse_iterator(iterator_type i) : current(i) {}
+        reverse_iterator(const iterator_type& rhs) : current(rhs.current) {}
+
+    public:
+        // 取出对应的正向迭代器
+        iterator_type base() const { return current; }
+
+        // 重载运载符
+        reference operator*() const {
+            // 实际对应正向迭代器的前一个位置
+            auto temp = current;
+            return *--temp;
+        }
+
+        pointer operator->() const {
+            return &(operator*());
+        }
+
+        // 前进(++)变为后退(--)
+        self& operator++() {
+            --current;
+            return *this;
+        }
+
+        self operator++(int) {
+            self temp = *this;
+            --current;
+            return temp;
+        }
+
+        // 后退(--)变为前进(++)
+        self& operator--() {
+            ++current;
+            return *this;
+        }
+
+        self operator--(int) {
+            self temp = *this;
+            ++current;
+            return temp;
+        }
+
+        self& operator+=(difference_type n) {
+            current -= n;
+            return *this;
+        }
+
+        self operator+(difference_type n) const {
+            return self(current - n);
+        }
+
+        self& operator-=(difference_type n) {
+            current += n;
+            return *this;
+        }
+
+        self operator-(difference_type n) const {
+            return self(current + n);
+        }
+
+        reference operator[](difference_type n) const {
+            return *(*this + n);
+        }
+
+    };
+
+    // 重载 operator-
+    template<typename Iterator>
+    typename reverse_iterator<Iterator>::difference_type
+    operator-(const reverse_iterator<Iterator>& lhs,
+              const reverse_iterator<Iterator>& rhs) {
+        return rhs.base() - lhs.base();
+    }
+
+    // 重载比较运算符
+    template<typename Iterator>
+    bool operator==(const reverse_iterator<Iterator>& lhs,
+                    const reverse_iterator<Iterator>& rhs) {
+        return lhs.base() == rhs.base();
+    }
+
+    template<typename Iterator>
+    bool operator<(const reverse_iterator<Iterator>& lhs,
+        const reverse_iterator<Iterator>& rhs) {
+        return rhs.base() < lhs.base();
+    }
+
+    template<typename Iterator>
+    bool operator!=(const reverse_iterator<Iterator>& lhs,
+        const reverse_iterator<Iterator>& rhs) {
+        return !(lhs == rhs);
+    }
+
+    template<typename Iterator>
+    bool operator>(const reverse_iterator<Iterator>& lhs,
+        const reverse_iterator<Iterator>& rhs) {
+        return rhs < lhs;
+    }
+
+    template<typename Iterator>
+    bool operator<=(const reverse_iterator<Iterator>& lhs,
+        const reverse_iterator<Iterator>& rhs) {
+        return !(rhs < lhs);
+    }
+
+    template<typename Iterator>
+    bool operator>=(const reverse_iterator<Iterator>& lhs,
+        const reverse_iterator<Iterator>& rhs) {
+        return !(lhs < rhs);
+    }
+
+} // namespace mystl
 
 #endif  //  MY_TINY_ITERATOR_H_
