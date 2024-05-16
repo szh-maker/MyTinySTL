d28824052ed4699304f552ed5d84a83d14b9c12f
szh-maker <2412192306@qq.com>
2024-05-16 23:13:29 +0800

    update

--- MyTinySTL/algobase.h
+++ MyTinySTL/algobase.h
@@ -70,6 +70,89 @@ OutputIter unchecked_copy_cat(InputIter first, InputIter last, OutputIter result
         *result = *first;
     }
     return result;
+};
+
+// random_access_iterator_tag 版本
+template<typename RandomIter, typename OutputIter>
+OutputIter unchecked_copy_cat(RandomIter first, RandomIter last, OutputIter result, mystl::random_access_iterator_tag) {
+    for(auto n = last - first; n > 0; --n, ++first, ++result) {
+        *result = *first;
+    }
+    return result;
+};
+
+template<typename InputIter, typename OutputIter>
+OutputIter unchecked_copy(InputIter first, InputIter last, OutputIter result) {
+    return unchecked_copy_cat(first, last, result, iterator_category(first));
+};
+
+// 为 trivially_copy_assignable 类型提供特化版本
+template<typename Tp, typename Up>
+typename std::enable_if<
+    std::is_same<typename std::remove_const<Tp>::type, Up>::value &&
+    std::is_trivially_copy_assignable<Up>::value,
+    Up*>::type
+unchecked_copy(Tp* first, Tp* last, Up* result) {
+    const auto n = static_cast<size_t>(last - first);
+    if(n != 0)
+        std::memmove(result, first, n * sizeof(Up));
+    return result + n;
+};
+
+template<typename InputIter, typename OutputIter>
+OutputIter copy(InputIter first, InputIter last, OutputIter result) {
+    return unchecked_copy(first, last, result);
+};
+
+/*********************************************************************/
+// copy_backward
+// 将[first, last)区间内的元素拷贝到[result - (last - first), result)内
+/*********************************************************************/
+// unchecked_copy_backward_cat 的 bidirectional_iterator_tag 版本
+template<typename BidirectionalIter1, typename BidirectionalIter2>
+BidirectionalIter2 unchecked_copy_backward_cat(BidirectionalIter1 first, BidirectionalIter1 last,
+    BidirectionalIter2 result, mystl::bidirectional_iterator_tag) {
+    while(first != last) {
+        *--result = *--last;
+    }
+    return result;
+}
+
+// unchecked_copy_backward_cat 的 random_access_iterator_tag 版本
+template<typename RandomIter1, typename BidirectionalIter2>
+BidirectionalIter2 unchecked_copy_backward_cat(RandomIter1 first, RandomIter1 last,
+    BidirectionalIter2 result, mystl::random_access_iterator_tag) {
+    for(auto n = last - first; n > 0; --n) {
+        *--result == *--last;
+    }
+    return result;
+}
+
+template<typename BidirectionalIter1, typename BidirectionalIter2>
+BidirectionalIter2 unchecked_copy_backward(BidirectionalIter1 first, BidirectionalIter1 last,
+    BidirectionalIter2 result) {
+    return unchecked_copy_backward_cat(first, last, result, iterator_category(first));
+}
+
+// 为 trivially_copy_assignable 类型提供特化版本
+template<typename Tp, typename Up>
+typename std::enable_if<
+    std::is_same<typename std::remove_const<Tp>::type, Up>::value &&
+    std::is_trivially_copy_assignable<Up>::value,
+    Up*>::type
+unchecked_copy_backward(Tp* first, Tp* last, Up* result) {
+    const auto n = static_cast<size_t>(last - first);
+    if(n != 0) {
+        result -= n;
+        std::memmove(result, first, n * sizeof(Up));
+    }
+    return result;
+}
+
+template<typename BidirectionalIter1, typename BidirectionalIter2>
+BidirectionalIter2 copy_backward(BidirectionalIter1 first, BidirectionalIter1 last, 
+    BidirectionalIter2 result) {
+    return unchecked_copy_backward(first, last, result);
 }
 
 }
